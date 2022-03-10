357ca00a5aa0f531f047ebf1fcd16a754c1d4469
Adnan-kreiker <adnankreiker@hotmail.com>
2022-03-10 23:25:15 +0200

    created home page template

---
+++ src/layouts/default.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { NMenu } from 'naive-ui'
+import type { MenuOption } from 'naive-ui'
+import { isDark } from '~/logic'
+const activeKey = ref<string | null>(null)
+const fontColor = computed(() => { return isDark.value ? 'white' : 'black' })
+
+const MenuOptions: MenuOption[] = [
+  {
+    label: 'Home',
+    key: 'Home',
+  },
+  {
+    label: 'Champions',
+    key: 'Champions',
+  },
+  {
+    label: 'Stats',
+    key: 'Stats',
+  },
+  {
+    label: 'Leaderboards',
+    key: 'Leaderboards',
+  },
+
+]
+</script>
+
+<template>
+  <main class="px-4 py-10  text-gray-700 dark:text-gray-200 min-h-screen">
+    <n-menu v-model="activeKey" mode="horizontal" :options="MenuOptions"></n-menu>
+    <!-- <n-button @click="toggleDark()">
+      Click me
+    </n-button> -->
+    <router-view />
+    <Footer></Footer>
+  </main>
+</template>
+
+<style>
+.n-menu-item-content-header{
+  color: v-bind(fontColor) !important;
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="flex flex-row justify-center h-full items-center">
+    <img src="/bg.webp" alt="">
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>

081f9e6efb3b39d9f1524d252c669220fa81efe5
Adnan-kreiker <adnankreiker@hotmail.com>
2022-03-11 22:59:09 +0200

    added champ-info page: lore, mini pic, info, tip and main pic

--- src/components.d.ts
+++ src/components.d.ts
@@ -4,15 +4,22 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
+    AttackIcon: typeof import('C:/Users/Adnan Kreiker/Documents/lol/my-vitespa-app/src/components/AttackIcon.vue')['default']
+    BrainIcon: typeof import('C:/Users/Adnan Kreiker/Documents/lol/my-vitespa-app/src/components/BrainIcon.vue')['default']
     CarbonDicomOverlay: typeof import('~icons/carbon/dicom-overlay')['default']
     CarbonLanguage: typeof import('~icons/carbon/language')['default']
     CarbonLogoGithub: typeof import('~icons/carbon/logo-github')['default']
     CarbonMoon: typeof import('~icons/carbon/moon')['default']
     CarbonScooter: typeof import('~icons/carbon/scooter')['default']
     CarbonSun: typeof import('~icons/carbon/sun')['default']
+    CheckMark: typeof import('C:/Users/Adnan Kreiker/Documents/lol/my-vitespa-app/src/components/CheckMark.vue')['default']
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
     Footer: typeof import('C:/Users/Adnan Kreiker/Documents/lol/my-vitespa-app/src/components/Footer.vue')['default']
+    MagicIcon: typeof import('C:/Users/Adnan Kreiker/Documents/lol/my-vitespa-app/src/components/MagicIcon.vue')['default']
     README: typeof import('C:/Users/Adnan Kreiker/Documents/lol/my-vitespa-app/src/components/README.md')['default']
+    ShieldIcon: typeof import('C:/Users/Adnan Kreiker/Documents/lol/my-vitespa-app/src/components/ShieldIcon.vue')['default']
+    SwordIcon: typeof import('C:/Users/Adnan Kreiker/Documents/lol/my-vitespa-app/src/components/SwordIcon.vue')['default']
+    WarningIcon: typeof import('C:/Users/Adnan Kreiker/Documents/lol/my-vitespa-app/src/components/WarningIcon.vue')['default']
   }
 }
 
--- src/pages/champion-info/[champion].vue
+++ src/pages/champion-info/[champion].vue
@@ -1,5 +1,12 @@
 <script setup lang="ts">
 import type { ChampionInfo } from '~/types'
+import CheckMark from '~/components/CheckMark.vue'
+import WarningIcon from '~/components/WarningIcon.vue'
+import AttackIcon from '~/components/AttackIcon.vue'
+import ShieldIcon from '~/components/ShieldIcon.vue'
+import MagicIcon from '~/components/MagicIcon.vue'
+import BrainIcon from '~/components/BrainIcon.vue'
+
 const route = useRoute()
 
 const champion = ref<null | ChampionInfo>(null)
@@ -12,8 +19,6 @@ async function getChampion() {
 
 getChampion()
 
-console.log(champion.value)
-
 </script>
 
 <template>
@@ -21,10 +26,95 @@ console.log(champion.value)
     <h1 class="text-3xl font-bold text-center mb-6">
       {{ champion.name }} the {{ champion.title }}
     </h1>
-    <p>
-      {{ champion.lore }}
-    </p>
+    <div class="flex flex-row items-center justify-center flex-wrap sm:flex-nowrap border-1 border-green-500 p-3">
+      <div class="h-full min-w-[98px] relative">
+        <img class="my-auto" height="98" :src="`http://ddragon.leagueoflegends.com/cdn/12.5.1/img/champion/${champion.id}.png`">
+        <div class="absolute bottom-1 right-0">
+          <p v-for="(tag, i) in champion.tags" :key="i" class="bg-dark-500  text-white text-xs p-0">
+            {{ tag }}
+          </p>
+        </div>
+      </div>
+      <p class="text-base text-center p-3">
+        {{ champion.lore }}
+      </p>
+    </div>
+
+    <div class="my-6">
+      <section>
+        <div class="flex flex-row flex-nowrap items-center">
+          <div class=" flex min-w-26 flex-row items-center">
+            <AttackIcon class="h-5 w-5"></AttackIcon>
+            <p class=" text-base md:text-lg mx-2 capitalize ">
+              Attack
+            </p>
+          </div>
+          <div :style="{width: champion.info.attack *10 +'%', maxWidth:70+'%'}" class="bg-red-500 h-4"></div>
+        </div>
+        <div class="flex flex-row items-center">
+          <div class=" flex min-w-26 flex-row items-center">
+            <ShieldIcon class="h-5 w-5"></ShieldIcon>
+            <p class=" text-base md:text-lg mx-2 capitalize ">
+              Defense
+            </p>
+          </div>
+          <div :style="{width: champion.info.defense *10 +'%', maxWidth:70+'%'}" class="bg-green-500 h-4"></div>
+        </div>
+        <div class="flex flex-row items-center">
+          <div class=" flex min-w-26 flex-row items-center">
+            <MagicIcon class="h-5 w-5"></MagicIcon>
+            <p class=" text-base md:text-lg mx-2 capitalize ">
+              Magic
+            </p>
+          </div>
+          <div :style="{width: champion.info.magic *10 +'%', maxWidth:70+'%'}" class="bg-purple-500 h-4"></div>
+        </div>
+        <div class="flex  flex-row items-center">
+          <div class=" flex min-w-26 flex-row items-center">
+            <BrainIcon class="h-5 w-5"></BrainIcon>
+            <p class=" text-base md:text-lg mx-2 capitalize">
+              Difficulty
+            </p>
+          </div>
+          <div :style="{width: champion.info.difficulty *10 +'%', maxWidth:70+'%'}" class="bg-pink-500 h-4"></div>
+        </div>
+      </section>
+    </div>
+
+    <div class="border border-green-500 my-3 p-3">
+      <h2 class="text-lg font-bold underline underline-green-500 underline-2">
+        Ally tips
+      </h2>
+      <ul>
+        <li v-for="tip in champion.allytips" :key="tip" class="text-base py-2 flex flex-row items-start ">
+          <div
+            class="h-4 w-6 mt-1 mr-1"
+          >
+            <CheckMark></CheckMark>
+          </div>
+          {{ tip }}
+        </li>
+      </ul>
+    </div>
+
+    <div class="border border-green-500 my-3 p-3">
+      <h2 class="text-lg font-bold underline underline-green-500 underline-2">
+        Enemy Tip
+      </h2>
+      <ul>
+        <li v-for="tip in champion.enemytips" :key="tip" class="text-base flex flex-row items-start py-2 ">
+          <div
+            class="h-4 w-6 mt-1 mr-1"
+          >
+            <WarningIcon></WarningIcon>
+          </div>
+          <span>{{ tip }}</span>
+        </li>
+      </ul>
+    </div>
+
     <img
+      class="mx-auto"
       :src="`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`"
     >
   </div>

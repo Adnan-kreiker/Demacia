b3886df88c69efb4552d08bd1d83a93b207d9dc0
Adnan-kreiker <adnankreiker@hotmail.com>
2022-03-16 00:11:35 +0200

    moved the api-key to .env file

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 4008
@@ -5,7 +6,7 @@ const qs = require('qs')
 const axios = require('axios')
 
 const url = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'
-const api_Key = 'RGAPI-d48b1891-f4ab-4aa2-a013-ff1ff389680d'
+const api_Key = process.env.API_KEY
 app.get('/', (req, res) => {
   res.set('Access-Control-Allow-Origin', '*')
   console.log(req.query)

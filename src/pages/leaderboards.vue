5145d804ef1a4c76260f7576fdd709ff542233dd
Adnan-kreiker <adnankreiker@hotmail.com>
2022-04-01 03:27:21 +0300

    Feat: added the ability to choose queue type in the leaderboards page

--- src/pages/leaderboards.vue
+++ src/pages/leaderboards.vue
@@ -1,37 +1,41 @@
 <script setup lang="ts">
 import {
-  QueueTypes,
   ChallengerPlayers,
   ChallengerPlayer,
   ChallengerPlayerWithIndex,
 } from "../../src/types";
-// import SummonersList from "~/components/SummonersList.vue";
-import { NPagination } from "naive-ui";
+import { NPagination, NSelect, NSpace, NH3, NText } from "naive-ui";
 import SummonersTableSkeleton from "~/components/SummonersTableSkeleton.vue";
 import SummonersTable from "~/components/SummonersTable.vue";
 
-const queueType = ref<QueueTypes>(QueueTypes.RANKED_SOLO_5x5);
-getChallengerPlayers();
+const options = [
+  {
+    label: "RANKED SOLO 5x5",
+    value: "RANKED_SOLO_5x5",
+  },
+  {
+    label: "RANKED FLEX SR",
+    value: "RANKED_FLEX_SR",
+  },
+];
+
+const queue = ref("RANKED_SOLO_5x5");
 const challengerPlayers = ref<ChallengerPlayer[] | []>([]);
 
+getChallengerPlayers();
+
 const sortedChallengerPlayers = computed<ChallengerPlayerWithIndex[] | []>(() => {
+  let start = page.value * 10;
   if (challengerPlayers.value) {
-    return (challengerPlayers.value as ChallengerPlayerWithIndex[])
+    return ([...challengerPlayers.value] as ChallengerPlayerWithIndex[])
       .sort((a, b) => b.leaguePoints - a.leaguePoints)
       .map((player, idx) => {
         return {
           ...player,
           idx: idx + 1,
         };
-      });
-  }
-  return [];
-});
-
-const currentPageData = computed(() => {
-  if (sortedChallengerPlayers.value.length > 0) {
-    let start = page.value * 10;
-    return sortedChallengerPlayers.value.slice(start - 10, start);
+      })
+      .slice(start - 10, start);
   }
   return [];
 });
@@ -39,32 +43,52 @@ const currentPageData = computed(() => {
 const page = ref(1);
 
 async function getChallengerPlayers() {
+  challengerPlayers.value = [];
   const res = await fetch(
-    `http://localhost:5000/api/get-challenger-players/${queueType.value}`
+    `http://localhost:5000/api/get-challenger-players/${queue.value}`
   );
   const data = (await res.json()) as ChallengerPlayers;
   challengerPlayers.value = data.entries;
 }
 
+const summonersTableKey = ref(0);
+
+watch(queue, () => {
+  getChallengerPlayers();
+  summonersTableKey.value++;
+});
+
+watch(page, () => {
+  summonersTableKey.value++;
+});
+
 const updatePage = (pageNumber: number) => {
   page.value = pageNumber;
+  summonersTableKey.value++;
 };
 </script>
 
 <template>
   <div class="min-h-screen">
+    <n-space>
+      <n-h3 class="mx-4" prefix="bar" align-text type="success">
+        <n-text type="success"> Choose queue type </n-text>
+      </n-h3>
+      <n-select class="min-w-56" v-model:value="queue" :options="options" />
+    </n-space>
     <summoners-table
-      v-if="challengerPlayers.length > 0"
-      :challenger-players="currentPageData"
-      :key="page"
+      v-if="challengerPlayers && challengerPlayers.length > 0"
+      :challenger-players="sortedChallengerPlayers"
+      :key="summonersTableKey"
       :page="page"
     ></summoners-table>
     <summoners-table-skeleton v-else></summoners-table-skeleton>
     <div class="flex justify-center mt-5">
       <n-pagination
+        v-if="challengerPlayers && challengerPlayers.length > 0"
         v-model="page"
         :default-page="1"
-        :page-count="30"
+        :page-count="Math.ceil(challengerPlayers.length / 10)"
         v-on:update-page="updatePage($event)"
       ></n-pagination>
     </div>

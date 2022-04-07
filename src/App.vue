8a4952991e37055522689f81e203d9d7a87e9232
Adnan-kreiker <adnankreiker@hotmail.com>
2022-04-07 22:51:11 +0300

    removed the readme.md file and renamed the project's name in app.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: "League of Legends cafe",
+  title: "League of Legends wikis",
   meta: [
     { name: "description", content: "All you need to know about League of Legends" },
   ],

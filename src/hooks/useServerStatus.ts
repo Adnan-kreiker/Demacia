34a7bfb3b72ff19c1cfaa2e5fb602cbe79f743b2
Adnan-kreiker <adnankreiker@hotmail.com>
2022-05-04 20:28:43 +0300

    refactor: refactor the borderColor into a separate method for a cleaner template

--- src/hooks/useServerStatus.ts
+++ src/hooks/useServerStatus.ts
@@ -3,9 +3,9 @@ const useServerStatus = () => {
   const servers = ['euw1', 'eun1', 'na1', 'kr', 'jp1', 'br1', 'tr1', 'ru', 'la1', 'la2', 'oc1'];
   const status = ref<ServerStatusData[]>([])
 
-  const getServerStatus = async () => {
+  const getServerStatus = () => {
     const tempData = ref<ServerStatusData[]>([])
-    await Promise.allSettled(
+    Promise.allSettled(
       servers.map(async (server) => {
         fetch(`${import.meta.env.VITE_URL}/api/get-server-status/${server}`)
           .then(res => res.json() as Promise<ServerStatusData>)
--- src/pages/status.vue
+++ src/pages/status.vue
@@ -3,8 +3,19 @@ import useServerStatus from "~/hooks/useServerStatus";
 import NDivider from "naive-ui/es/divider/src/Divider";
 import { NText } from "naive-ui/es/typography";
 import { NH1 } from "naive-ui/es/typography";
+import { Maintenance } from "~/types";
 
 const { status } = useServerStatus()
+
+const borderColor = (stat1: [] | Maintenance[], stat2: [] | Maintenance[]) => {
+  if (!stat1.length && !stat2.length) {
+    return "border-teal-400"
+  } else if (stat1.length || stat2.length) {
+    return "border-orange-400"
+  } else {
+    return "border-red-400"
+  }
+}
 
 </script>
 
@@ -14,9 +25,8 @@ const { status } = useServerStatus()
       <n-text type="primary" class="text-4xl"> Servers' Status </n-text>
     </n-h1>
     <div v-if="status" class="flex flex-row flex-wrap gap-4 justify-center md:w-[80%] mx-auto">
-      <div class="text-base my-4 border-1 p-4 w-auto"
-        :class="{ 'border-teal-400': !stat.maintenances.length && !stat.incidents.length, 'border-red-500': stat.maintenances.length || stat.incidents.length }"
-        v-for="stat in status" :key="stat.id">
+      <div class="text-base my-4 border-1 p-4 w-auto" v-for="stat in status" :key="stat.id"
+        :class="borderColor(stat.incidents, stat.maintenances)">
         <p class="font-bold text-lg mb-4">{{ stat.name }}</p>
         <div v-if="stat.maintenances.length">
           <p>Maintenance</p>

5ddcf2dd3249fa5a1271796aaf37239bd0fee0ca
Adnan-kreiker <adnankreiker@hotmail.com>
2022-05-04 22:10:12 +0300

    feat: add skeletons to the status page

--- src/hooks/useServerStatus.ts
+++ src/hooks/useServerStatus.ts
@@ -1,4 +1,6 @@
 import type { ServerStatusData } from './../types';
+
+
 const useServerStatus = () => {
   const servers = ['euw1', 'eun1', 'na1', 'kr', 'jp1', 'br1', 'tr1', 'ru', 'la1', 'la2', 'oc1'];
   const status = ref<ServerStatusData[]>([])
@@ -6,14 +8,14 @@ const useServerStatus = () => {
   const getServerStatus = () => {
     const tempData = ref<ServerStatusData[]>([])
     Promise.allSettled(
-      servers.map(async (server) => {
+      servers.map((server) => {
         fetch(`${import.meta.env.VITE_URL}/api/get-server-status/${server}`)
           .then(res => res.json() as Promise<ServerStatusData>)
           .then(data => tempData.value.push(data))
           .catch(err => console.error(err))
       }))
-      .then(() => status.value = tempData.value)
       .catch(err => console.error(err))
+    status.value = tempData.value
   }
 
   getServerStatus()
--- src/pages/status.vue
+++ src/pages/status.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import useServerStatus from "~/hooks/useServerStatus";
 import NDivider from "naive-ui/es/divider/src/Divider";
+import NSkeleton from "naive-ui/es/skeleton/src/Skeleton";
 import { NText } from "naive-ui/es/typography";
 import { NH1 } from "naive-ui/es/typography";
 import { Maintenance } from "~/types";
@@ -24,12 +25,12 @@ const borderColor = (stat1: [] | Maintenance[], stat2: [] | Maintenance[]) => {
     <n-h1>
       <n-text type="primary" class="text-4xl"> Servers' Status </n-text>
     </n-h1>
-    <div v-if="status" class="flex flex-row flex-wrap gap-4 justify-center md:w-[80%] mx-auto">
-      <div class="text-base my-4 border-1 p-4 w-auto" v-for="stat in status" :key="stat.id"
+    <div v-if="status.length" class="flex flex-row flex-wrap gap-4 justify-center md:w-[80%] mx-auto">
+      <div class="text-base min-w-[270px] my-4 border-1 p-4 " v-for="stat in status" :key="stat.id"
         :class="borderColor(stat.incidents, stat.maintenances)">
-        <p class="font-bold text-lg mb-4">{{ stat.name }}</p>
+        <p class="font-bold text-2xl mb-4">{{ stat.name }}</p>
         <div v-if="stat.maintenances.length">
-          <p>Maintenance</p>
+          <p class="text-lg font-bold">Maintenance:</p>
           <div v-for="incident in stat.maintenances">
             <p v-for="description in incident.updates">
               {{ description.translations[0].content }}
@@ -41,8 +42,8 @@ const borderColor = (stat1: [] | Maintenance[], stat2: [] | Maintenance[]) => {
         <n-divider class="divider-class"></n-divider>
 
         <div v-if="stat.incidents.length">
-          <p class="text-base">Incidents</p>
-          <div v-for="incident in stat.incidents">
+          <p class="text-lg font-bold">Incidents:</p>
+          <div class="mb-2" v-for="incident in stat.incidents">
             <li class="ml-4" v-for="description in incident.updates">
               {{ description.translations[0].content }}
             </li>
@@ -50,6 +51,9 @@ const borderColor = (stat1: [] | Maintenance[], stat2: [] | Maintenance[]) => {
         </div>
         <div v-else>No incidents were detected</div>
       </div>
+    </div>
+    <div v-else class="flex flex-row flex-wrap gap-4 justify-center">
+      <n-skeleton v-for="n in 11" :key="n" height="175px" width="270px" />
     </div>
   </div>
 </template>

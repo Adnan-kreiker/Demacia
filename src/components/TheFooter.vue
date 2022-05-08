951c7bd81bee4a5cd16e151f4137a534c73e5c8a
Adnan-kreiker <adnankreiker@hotmail.com>
2022-05-08 21:13:55 +0300

    edited the footer text

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -34,7 +34,7 @@
 
     <div class="text-center text-white p-4" style="background-color: rgba(0, 0, 0, 0.2)">
       © 2022 Copyright:
-      <a class="text-white" href="https://adnan-kreiker.netlify.app/">League of Legends Wikis</a>
+      <a class="text-white" href="https://adnan-kreiker.netlify.app/">Demacia ⚔</a>
       <p data-testid="made-by" class="text-sm">By Adnan Kreiker 🚀 🦄</p>
     </div>
   </footer>

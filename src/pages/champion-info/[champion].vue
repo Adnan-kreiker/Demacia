069215a437dad8c126f481da37d1e8372207ce04
Adnan-kreiker <adnankreiker@hotmail.com>
2022-05-23 23:12:02 +0300

    fix: adjusted the width of the skeleton in the chmpion's page

--- src/pages/champion-info/[champion].vue
+++ src/pages/champion-info/[champion].vue
@@ -139,178 +139,180 @@ watch(champion, () => {
 </script>
 
 <template>
-  <div v-if="champion" class="max-w-[1000px] mx-auto">
-    <h1 class="text-3xl font-bold text-center mb-6">
-      {{ champion.name }} the {{ champion.title }}
-    </h1>
-    <div class="flex flex-row items-center justify-center flex-wrap sm:flex-nowrap border-1 rounded-md border-green-500 p-3">
-      <div class="h-full min-w-[98px] relative">
-        <img
-          class="my-auto"
-          height="98"
-          :src="`https://ddragon.leagueoflegends.com/cdn/${patchVersion}/img/champion/${champion.id}.png`"
-        >
-        <div class="absolute bottom-1 right-0">
-          <p v-for="(tag, i) in champion.tags" :key="i" class="bg-dark-500 text-white text-xs p-0">
-            {{ tag }}
-          </p>
+  <div class="max-w-[1000px] mx-auto">
+    <div v-if="champion">
+      <h1 class="text-3xl font-bold text-center mb-6">
+        {{ champion.name }} the {{ champion.title }}
+      </h1>
+      <div class="flex flex-row items-center justify-center flex-wrap sm:flex-nowrap border-1 rounded-md border-green-500 p-3">
+        <div class="h-full min-w-[98px] relative">
+          <img
+            class="my-auto"
+            height="98"
+            :src="`https://ddragon.leagueoflegends.com/cdn/${patchVersion}/img/champion/${champion.id}.png`"
+          >
+          <div class="absolute bottom-1 right-0">
+            <p v-for="(tag, i) in champion.tags" :key="i" class="bg-dark-500 text-white text-xs p-0">
+              {{ tag }}
+            </p>
+          </div>
+        </div>
+        <p class="text-base text-center p-3">
+          {{ champion.lore }}
+        </p>
+      </div>
+
+      <div v-if="champion.info.attack && champion.info.defense" class="my-6">
+        <section>
+          <div class="flex flex-row flex-nowrap items-center">
+            <div class="flex min-w-26 flex-row items-center">
+              <AttackIcon class="h-5 w-5" />
+              <p class="text-base md:text-lg mx-2 capitalize">
+                Attack
+              </p>
+            </div>
+            <div :style="{ width: `${champion.info.attack * 10}%`, maxWidth: `${70}%` }" class="bg-red-500 h-4" />
+          </div>
+
+          <div class="flex flex-row items-center">
+            <div class="flex min-w-26 flex-row items-center">
+              <ShieldIcon class="h-5 w-5" />
+              <p class="text-base md:text-lg mx-2 capitalize">
+                Defense
+              </p>
+            </div>
+            <div :style="{ width: `${champion.info.defense * 10}%`, maxWidth: `${70}%` }" class="bg-green-500 h-4" />
+          </div>
+
+          <div class="flex flex-row items-center">
+            <div class="flex min-w-26 flex-row items-center">
+              <MagicIcon class="h-5 w-5" />
+              <p class="text-base md:text-lg mx-2 capitalize">
+                Magic
+              </p>
+            </div>
+            <div :style="{ width: `${champion.info.magic * 10}%`, maxWidth: `${70}%` }" class="bg-purple-500 h-4" />
+          </div>
+
+          <div class="flex flex-row items-center">
+            <div class="flex min-w-26 flex-row items-center">
+              <BrainIcon class="h-5 w-5" />
+              <p class="text-base md:text-lg mx-2 capitalize">
+                Difficulty
+              </p>
+            </div>
+            <div :style="{ width: `${champion.info.difficulty * 10}%`, maxWidth: `${70}%` }" class="bg-pink-500 h-4" />
+          </div>
+        </section>
+      </div>
+
+      <div v-if="champion.allytips.length" class="border rounded-md border-green-500 my-3 p-3">
+        <h2 class="text-lg font-bold underline underline-green-500 underline-2">
+          Ally tips
+        </h2>
+
+        <ul>
+          <li v-for="tip in champion.allytips" :key="tip" class="text-base py-2 flex flex-row items-start">
+            <div class="h-4 w-6 mt-1 mr-1">
+              <CheckMark />
+            </div>
+            {{ tip }}
+          </li>
+        </ul>
+      </div>
+
+      <div v-if="champion.enemytips.length" class="border rounded-md border-green-500 my-3 p-3">
+        <h2 class="text-lg font-bold underline underline-green-500 underline-2">
+          Enemy Tip
+        </h2>
+
+        <ul>
+          <li v-for="tip in champion.enemytips" :key="tip" class="text-base flex flex-row items-start py-2">
+            <div class="h-4 w-6 mt-1 mr-1">
+              <WarningIcon />
+            </div>
+            <span>{{ tip }}</span>
+          </li>
+        </ul>
+      </div>
+
+      <div class="flex flex-row flex-wrap gap-1 justify-center">
+        <n-data-table :columns="tableColumns" :data="champStats" :max-height="250" virtual-scroll class="my-8 md:w-1/2" />
+        <div class="md:w-1/2 mt-8 md:h-[300px]">
+          <img
+            class="mx-auto h-full object-cover"
+            :src="`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`"
+          >
         </div>
       </div>
-      <p class="text-base text-center p-3">
-        {{ champion.lore }}
-      </p>
-    </div>
 
-    <div v-if="champion.info.attack && champion.info.defense" class="my-6">
       <section>
-        <div class="flex flex-row flex-nowrap items-center">
-          <div class="flex min-w-26 flex-row items-center">
-            <AttackIcon class="h-5 w-5" />
-            <p class="text-base md:text-lg mx-2 capitalize">
-              Attack
-            </p>
+        <h2 class="text-4xl font-bold text-center text-green-400 my-8">
+          Abilities
+        </h2>
+        <div>
+          <div v-for="(spell, i) in spells" :key="i" class="flex flex-row my-6 border-1 border-white p-3">
+            <div class="min-w-[60px]">
+              <img
+                height="50"
+                width="50"
+                class=""
+                :src="`https://ddragon.leagueoflegends.com/cdn/${patchVersion}/img/spell/${spell.image.full}`"
+                alt=""
+              >
+            </div>
+            <div>
+              <h2 class="text-base font-bold">
+                {{ spell.name }}
+              </h2>
+              <p v-html="spell.description" />
+              <p
+                v-if="spells2"
+                v-html="replaceEffectsWithValues(replaceVarsWithCorrectSpellsValues(spells2[i].tip), i)"
+              />
+            <!-- <p v-if="effects">{{ effectsValueExtractor(effects)[i] }}</p> -->
+            </div>
           </div>
-          <div :style="{ width: `${champion.info.attack * 10}%`, maxWidth: `${70}%` }" class="bg-red-500 h-4" />
         </div>
-
-        <div class="flex flex-row items-center">
-          <div class="flex min-w-26 flex-row items-center">
-            <ShieldIcon class="h-5 w-5" />
-            <p class="text-base md:text-lg mx-2 capitalize">
-              Defense
-            </p>
+        <h2 class="text-4xl font-bold text-center text-green-400 my-8">
+          Skins
+        </h2>
+        <div class="flex flex-row justify-center">
+          <div>
+            <n-carousel
+              effect="card"
+              prev-slide-style="transform: translateX(-150%) translateZ(-800px);"
+              next-slide-style="transform: translateX(50%) translateZ(-800px);"
+              style="height: 440px; width: 100%"
+              draggable
+            >
+              <n-carousel-item
+                v-for="skin in champion.skins"
+                :key="skin.id"
+                :style="{ width: '80%' }"
+              >
+                <img :src="`https://ddragon.canisback.com/img/champion/loading/${champion!.id}_${skin.num}.jpg`" :alt="skin.name">
+                <p class="font-mono text-center text-base overflow-visible mt-2">
+                  {{ skin.name }}
+                </p>
+              </n-carousel-item>
+            </n-carousel>
           </div>
-          <div :style="{ width: `${champion.info.defense * 10}%`, maxWidth: `${70}%` }" class="bg-green-500 h-4" />
-        </div>
-
-        <div class="flex flex-row items-center">
-          <div class="flex min-w-26 flex-row items-center">
-            <MagicIcon class="h-5 w-5" />
-            <p class="text-base md:text-lg mx-2 capitalize">
-              Magic
-            </p>
-          </div>
-          <div :style="{ width: `${champion.info.magic * 10}%`, maxWidth: `${70}%` }" class="bg-purple-500 h-4" />
-        </div>
-
-        <div class="flex flex-row items-center">
-          <div class="flex min-w-26 flex-row items-center">
-            <BrainIcon class="h-5 w-5" />
-            <p class="text-base md:text-lg mx-2 capitalize">
-              Difficulty
-            </p>
-          </div>
-          <div :style="{ width: `${champion.info.difficulty * 10}%`, maxWidth: `${70}%` }" class="bg-pink-500 h-4" />
         </div>
       </section>
     </div>
-
-    <div v-if="champion.allytips.length" class="border rounded-md border-green-500 my-3 p-3">
-      <h2 class="text-lg font-bold underline underline-green-500 underline-2">
-        Ally tips
-      </h2>
-
-      <ul>
-        <li v-for="tip in champion.allytips" :key="tip" class="text-base py-2 flex flex-row items-start">
-          <div class="h-4 w-6 mt-1 mr-1">
-            <CheckMark />
-          </div>
-          {{ tip }}
-        </li>
-      </ul>
+    <div v-else class="flex flex-col gap-2 mt-8 ">
+      <n-skeleton class="mx-auto" height="36px" width="50%" />
+      <n-skeleton height="150px" width="100%" />
+      <n-skeleton height="28px" width="70%" />
+      <n-skeleton height="28px" width="40%" />
+      <n-skeleton height="28px" width="30%" />
+      <n-skeleton height="28px" width="60%" />
+      <n-skeleton height="198px" width="100%" />
+      <n-skeleton height="140px" width="100%" />
+      <n-skeleton height="363px" width="100%" />
+      <n-skeleton height="681px" width="100%" />
     </div>
-
-    <div v-if="champion.enemytips.length" class="border rounded-md border-green-500 my-3 p-3">
-      <h2 class="text-lg font-bold underline underline-green-500 underline-2">
-        Enemy Tip
-      </h2>
-
-      <ul>
-        <li v-for="tip in champion.enemytips" :key="tip" class="text-base flex flex-row items-start py-2">
-          <div class="h-4 w-6 mt-1 mr-1">
-            <WarningIcon />
-          </div>
-          <span>{{ tip }}</span>
-        </li>
-      </ul>
-    </div>
-
-    <div class="flex flex-row flex-wrap gap-1 justify-center">
-      <n-data-table :columns="tableColumns" :data="champStats" :max-height="250" virtual-scroll class="my-8 md:w-1/2" />
-      <div class="md:w-1/2 mt-8 md:h-[300px]">
-        <img
-          class="mx-auto h-full object-cover"
-          :src="`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_0.jpg`"
-        >
-      </div>
-    </div>
-
-    <section>
-      <h2 class="text-4xl font-bold text-center text-green-400 my-8">
-        Abilities
-      </h2>
-      <div>
-        <div v-for="(spell, i) in spells" :key="i" class="flex flex-row my-6 border-1 border-white p-3">
-          <div class="min-w-[60px]">
-            <img
-              height="50"
-              width="50"
-              class=""
-              :src="`https://ddragon.leagueoflegends.com/cdn/${patchVersion}/img/spell/${spell.image.full}`"
-              alt=""
-            >
-          </div>
-          <div>
-            <h2 class="text-base font-bold">
-              {{ spell.name }}
-            </h2>
-            <p v-html="spell.description" />
-            <p
-              v-if="spells2"
-              v-html="replaceEffectsWithValues(replaceVarsWithCorrectSpellsValues(spells2[i].tip), i)"
-            />
-            <!-- <p v-if="effects">{{ effectsValueExtractor(effects)[i] }}</p> -->
-          </div>
-        </div>
-      </div>
-      <h2 class="text-4xl font-bold text-center text-green-400 my-8">
-        Skins
-      </h2>
-      <div class="flex flex-row justify-center">
-        <div>
-          <n-carousel
-            effect="card"
-            prev-slide-style="transform: translateX(-150%) translateZ(-800px);"
-            next-slide-style="transform: translateX(50%) translateZ(-800px);"
-            style="height: 440px; width: 100%"
-            draggable
-          >
-            <n-carousel-item
-              v-for="skin in champion.skins"
-              :key="skin.id"
-              :style="{ width: '80%' }"
-            >
-              <img :src="`https://ddragon.canisback.com/img/champion/loading/${champion!.id}_${skin.num}.jpg`" :alt="skin.name">
-              <p class="font-mono text-center text-base overflow-visible mt-2">
-                {{ skin.name }}
-              </p>
-            </n-carousel-item>
-          </n-carousel>
-        </div>
-      </div>
-    </section>
-  </div>
-  <div v-else class="flex flex-col gap-2 mt-8 ">
-    <n-skeleton class="mx-auto" height="36px" width="50%" />
-    <n-skeleton height="150px" width="100%" />
-    <n-skeleton height="28px" width="70%" />
-    <n-skeleton height="28px" width="40%" />
-    <n-skeleton height="28px" width="30%" />
-    <n-skeleton height="28px" width="60%" />
-    <n-skeleton height="198px" width="100%" />
-    <n-skeleton height="140px" width="100%" />
-    <n-skeleton height="363px" width="100%" />
-    <n-skeleton height="681px" width="100%" />
   </div>
 </template>
 

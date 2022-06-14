4f0e3875a7fa731c4b50c0cb18f2d7f0ca5dc542
Adnan-kreiker <adnankreiker@hotmail.com>
2022-06-15 00:12:12 +0300

    refactor: refactor the status page to use the ServerStatusCard

--- src/pages/status.vue
+++ src/pages/status.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { NDivider, NSkeleton } from 'naive-ui'
+import { NSkeleton } from 'naive-ui'
 import useServerStatus from '~/hooks/useServerStatus'
-import type { AnyElementOf, FilterOption, Maintenance, Servers, Title } from '~/types'
+import type { AnyElementOf, FilterOption, Servers } from '~/types'
 import FilterComponent from '~/components/FilterComponent.vue'
 
 const { status } = useServerStatus()
@@ -25,21 +25,6 @@ const filteredServers = computed(() => {
 
   return status.value
 })
-
-const borderColor = (stat1: [] | Maintenance[], stat2: [] | Maintenance[]) => {
-  if (Array.isArray(stat1) && Array.isArray(stat2)) {
-    if (!stat1.length && !stat2.length)
-      return 'shadow-teal-400'
-
-    else if (stat1.length || stat2.length)
-      return 'shadow-orange-400'
-
-    else
-      return 'shadow-red-400'
-  }
-}
-
-const findEnglishTranslation = (translations: Title[]) => translations.find(translation => translation.locale === 'en_US')
 </script>
 
 <template>
@@ -54,47 +39,9 @@ const findEnglishTranslation = (translations: Title[]) => translations.find(tran
         <span class="text-xl">Filter by Server</span>
         <FilterComponent class="filter-Component" :filter-options="filterOptions()" :current-filter="currentFilter" @update-filter="currentFilter = $event as AnyElementOfServers" />
       </div>
-      <Transition name="fadeMenu" appear>
-        <div class="flex flex-row flex-wrap gap-4 justify-center md:w-[80%] mx-auto">
-          <div
-            v-for="stat in filteredServers"
-            :key="stat.id"
-            class="text-base min-w-[270px] transition-all animate-fade-in duration-300 ease-linear bg-dark-900 my-4 rounded-md p-4 shadow-t-md"
-            :class="borderColor(stat.incidents, stat.maintenances)"
-          >
-            <p class="font-bold text-2xl mb-4">
-              {{ stat.name }}
-            </p>
-            <div v-if="stat.maintenances && stat.maintenances.length">
-              <p class="text-lg font-bold">
-                Maintenance:
-              </p>
-              <div v-for="incident in stat.maintenances" :key="incident.id">
-                <p v-for="description in incident.updates" :key="description.id">
-                  {{ findEnglishTranslation(description.translations)?.content }}
-                </p>
-              </div>
-            </div>
-            <div v-else class="flex flex-row gap-2 items-center">
-              <span>Server is up and Running</span>
-            </div>
-            <n-divider class="divider-class" />
-            <div v-if="stat.incidents && stat.incidents.length">
-              <p class="text-lg font-bold">
-                Incidents:
-              </p>
-              <div v-for="incident in stat.incidents" :key="incident.id" class="mb-2">
-                <li v-for="description in incident.updates" :key="description.id" class="ml-4">
-                  {{ description.translations[0].content }}
-                </li>
-              </div>
-            </div>
-            <div v-else>
-              No incidents were detected
-            </div>
-          </div>
-        </div>
-      </Transition>
+      <div class="flex flex-row flex-wrap gap-4 justify-center md:w-[80%] mx-auto">
+        <ServerStatusCard v-for="stat in filteredServers" :key="stat.id" :stat="stat" />
+      </div>
     </div>
     <div v-else class=" md:w-[80%] mx-auto">
       <n-skeleton height="37px" width="320px" class="my-6 mx-auto" />

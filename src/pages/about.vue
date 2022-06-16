951723aa2f4a25df8f4d6703debc336f48ee6318
Adnan Kreiker <adnankreiker@hotmail.com>
2022-06-16 11:42:55 -0700

    feat: add About page

---
+++ src/pages/about.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="max-w-[770px] mx-auto">
+    <div class="gradient-border w-max mb-4 mx-auto">
+      <h2 class="text-3xl text-center border rounded-md border-dark-200 p-3 bg-dark-900 w-max mx-auto px-8">
+        About
+      </h2>
+    </div>
+    <div class="gradient-border mb-4 rounded-sm">
+      <p class="text-base border rounded-md border-dark-200 p-3 bg-dark-900">
+        I created this project to practice the latest Vue.js syntax with the script setup and TypeScript. Also, because I'm a big League of Legends fan.<br>
+        <br>
+        <em class="text-gray-400">You got interesting ideas for new features? feel free to contribute to this project!</em>
+      </p>
+    </div>
+    <img class="mx-auto w-full rounded-sm" src="/vue3.webp" alt="">
+  </div>
+</template>
+
+<style scoped>
+
+</style>

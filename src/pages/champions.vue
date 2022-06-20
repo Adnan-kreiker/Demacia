95d9a3048762058bcec628303cb2fd60ae3bb2be
Adnan-kreiker <adnankreiker@hotmail.com>
2022-06-20 15:56:45 +0200

    feat: add filtering mechanism

--- src/pages/champions.vue
+++ src/pages/champions.vue
@@ -1,42 +1,85 @@
 <script setup lang="ts">
-import { NSkeleton } from 'naive-ui'
-import useChampions from '~/composables/useChampions'
-import ChampionCard from '~/components/ChampionCard.vue'
-
-// const { width } = useWindowSize()
-
-// const itemSize = computed(() => {
-//   if (width.value < 252)
-//     return 75
-
-//   if (width.value <= 365)
-//     return 80
-
-//   if (width.value < 480)
-//     return 55
-
-//   if (width.value < 594)
-//     return 40
-
-//   if (width.value < 708)
-//     return 35
-
-//   if (width.value < 886)
-//     return 30
-
-//   if (width.value < 1000)
-//     return 25
-
-//   if (width.value < 1228)
-//     return 20
-
-//   if (width.value > 1680)
-//     return 12
-
-//   return 15
-// })
+import { NInput, NSkeleton } from 'naive-ui'
+import type { Champion } from '~/types'
 
 const { championsArray } = useChampions()
+
+const champions = ref<null | Champion[]>(null)
+
+const search = ref('')
+
+watch(championsArray, () => {
+  champions.value = championsArray.value
+}, { immediate: true })
+
+type Roles = 'Fighter' | 'Marksman' | 'Tank' | 'Support' | 'Mage' | 'Assassin'
+
+const roles = ref([
+  {
+    name: 'Fighter',
+    value: false,
+  },
+  {
+    name: 'Marksman',
+    value: false,
+  },
+  {
+    name: 'Tank',
+    value: false,
+  },
+  {
+    name: 'Support',
+    value: false,
+  },
+  {
+    name: 'Mage',
+    value: false,
+  },
+  {
+    name: 'Assassin',
+    value: false,
+  },
+])
+
+const pickedRole = ref<Roles[]>([])
+
+function filterRoles(champs: Champion[], roles: Roles[]): Champion[] {
+  if (!roles.length)
+    return []
+  const currentRole = roles[0]
+  roles = roles.slice(1)
+  return champs.filter((champ) => {
+    return champ.tags.includes(currentRole)
+  }).concat(filterRoles(champs, roles))
+}
+
+const filteredChampions = computed(() => {
+  if (!championsArray.value)
+    return []
+
+  if (pickedRole.value.length && champions.value)
+    return filterRoles(champions.value, pickedRole.value)
+
+  if (search.value && champions.value) {
+    return champions.value.filter((champion) => {
+      return champion.name.toLowerCase().includes(search.value)
+    })
+  }
+  return championsArray.value
+})
+
+function triggerFilter(role: Roles) {
+  if (pickedRole.value.includes(role))
+    pickedRole.value = pickedRole.value.filter(r => r !== role)
+
+  else
+    pickedRole.value.push(role)
+}
+
+function filterStyle(role: Roles) {
+  if (pickedRole.value.includes(role))
+    return '!bg-teal-500'
+}
 </script>
 
 <template>
@@ -44,22 +87,27 @@ const { championsArray } = useChampions()
     <h1 class="text-green-300 text-4xl font-bold text-center mt-0 mb-8">
       Champions
     </h1>
-    <div v-if="championsArray" class="flex flex-row flex-wrap gap-3 justify-center">
-      <!-- <RecycleScroller class="scroll-class" :items="championsArray" :item-size="itemSize" key-field="id"> -->
-      <!-- <template #default="{ item }"> -->
-      <ChampionCard v-for="champ in championsArray" :key="champ.id" :champ="champ" />
-      <!-- </template> -->
-      <!-- </RecycleScroller> -->
+    <n-input
+      v-model:value="search" type="text" clearable class="max-w-60 mx-auto block mb-6"
+      placeholder="Search"
+    />
+    <div class="flex gap-3 justify-center mb-5">
+      <p
+        v-for="role in roles" :key="role.name"
+        class="border border-white bg-gray-600 w-min px-3 py-1 rounded-sm text-white"
+        :class="filterStyle(role.name as Roles)" @click="triggerFilter(role.name as Roles)"
+      >
+        {{ role.name }}
+      </p>
+    </div>
+    <div v-if="championsArray" class="flex flex-row flex-wrap gap-4 justify-center">
+      <ChampionCard v-for="champ in filteredChampions" :key="champ.id" :champ="champ" />
     </div>
     <div v-else class="flex flex-row flex-wrap gap-6 justify-center">
       <n-skeleton v-for="skeleton in 70" :key="skeleton" height="138px" width="98px" />
     </div>
   </div>
 </template>
-
-<style >
-
-</style>
 
 <route lang="yaml">
 meta:

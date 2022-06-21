51250b6d8938c83d37e0a950eb41992150121369
Adnan-kreiker <adnankreiker@hotmail.com>
2022-06-21 22:58:34 +0200

    style : add vue, vite, and TS icons

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,28 +4,32 @@ import VercelLogo from '~/components/Icons/VercelLogo.vue'
 </script>
 
 <template>
-  <div class="pb-8">
+  <div class="pb-8 my-auto">
     <h2 class="text-center text-4xl mb-6 font-mono font-bold">
       Demacia
     </h2>
     <div class="sm:max-w-[90%] md:max-w-[70%] lg:max-w-[50%] mx-auto">
-      <div
-        class="flex flex-col justify-center items-center pt-4 pb-7 border-1 bg-dark-900
-      border-gray-600 w-full shadow-t-md  shadow-gray-400"
-      >
-        <img
-          fetchpriority="high"
-          height="280"
-          width="250"
-          class="object-cover yummi-img h-70 w-[250px] my-4 mx-auto"
-          src="/yummi.webp"
-          alt="Yummi hovering"
+      <div class="gradient-border p-1 rounded-md">
+        <div
+          class="flex flex-col justify-center items-center pt-4 pb-7 bg-dark-900 w-full shadow-t-md "
         >
-        <SearchForSummoner />
+          <img
+            fetchpriority="high" height="280" width="250" class="object-cover yummi-img h-70 w-[250px] my-4 mx-auto"
+            src="/yummi.webp" alt="Yummi hovering"
+          >
+          <SearchForSummoner />
+        </div>
       </div>
-      <div class="flex flex-row items-center justify-center">
-        <span class="text-lg mr-4 text-gray-400">Powered by </span>
-        <VercelLogo class="h-27 w-27" />
+      <div class="flex flex-col justify-center items-center mt-4">
+        <div class="flex  justify-center items-center">
+          <span class="text-lg mr-4 text-gray-400">Powered by </span>
+          <VercelLogo class="h-27 w-27 scale-on-hover" />
+        </div>
+        <div class="flex">
+          <logos-vue class="h-8 w-8 ml-7 scale-on-hover" />
+          <logos-vitejs class="h-8 w-8 ml-7 scale-on-hover" />
+          <logos-typescript-icon class="h-8 w-8 ml-7 scale-on-hover" />
+        </div>
       </div>
     </div>
   </div>

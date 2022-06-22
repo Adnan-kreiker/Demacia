117f6a24d9cfb976caca1e211ccb38a594ab155e
Adnan-kreiker <adnankreiker@hotmail.com>
2022-06-22 21:28:55 +0200

    feat: add useSummonerLeague composable

---
+++ src/composables/useSummonerLeague.ts
@@ -0,0 +1,19 @@
+import type { MaybeRef, RegionParam, SummonerLeagueInfo } from '~/types'
+
+const useSummonerLeague = async (region: MaybeRef<RegionParam>, leagueId: MaybeRef<string>) => {
+  const leagueInfo = ref<null | SummonerLeagueInfo>(null)
+  try {
+    const res = await fetch(`${import.meta.env.VITE_URL}/api/get-summoner-league/${unref(region)}?${unref(leagueId)}`)
+    const data = await res.json()
+    leagueInfo.value = data
+  }
+  catch (error) {
+    console.error(error)
+  }
+
+  return {
+    leagueInfo,
+  }
+}
+
+export default useSummonerLeague

cb4356fc74afb504e20afa9c491f47ee3eb7d0e7
Adnan-kreiker <adnankreiker@hotmail.com>
2022-06-24 11:19:30 +0200

    refactor: add summonerLeagueInfo component

---
+++ src/components/SummonerLeagueInformation.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import type { Ref } from 'vue'
+import useSummonerLeague from '~/composables/useSummonerLeague'
+import { useRegionStore } from '~/stores/region'
+import { usePlayerLeagueIdStore } from '~/stores/playerLeagueId'
+
+const regionStore = useRegionStore()
+const playerLeagueIdStore = usePlayerLeagueIdStore()
+
+const { region } = storeToRefs(regionStore)
+const { id: playerLeagueId } = storeToRefs(playerLeagueIdStore)
+
+const { leagueInfo } = await useSummonerLeague(region, playerLeagueId as Ref<string>)
+</script>
+
+<template>
+  <p v-if="leagueInfo && playerLeagueId">
+    {{ leagueInfo }}
+  </p>
+</template>
+

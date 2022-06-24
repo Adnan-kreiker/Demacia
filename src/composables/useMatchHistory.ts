d83f7537ff8f5a397475c30f30420b0edc45fea7
Adnan-kreiker <adnankreiker@hotmail.com>
2022-06-24 11:18:54 +0200

    refactor: add MaybeRef

--- src/composables/useMatchHistory.ts
+++ src/composables/useMatchHistory.ts
@@ -1,25 +1,25 @@
 import type { Ref } from 'vue'
 import { regionParamToContinentMapper } from '../../utils'
-import type { MatchInfo } from '~/types'
+import type { MatchInfo, MaybeRef } from '~/types'
 
 const error = ref(false)
 const loading = ref(false)
 
-const useMatchHistory = (puuid: string, region: string, start: Ref<number>) => {
+const useMatchHistory = (puuid: MaybeRef<string>, region: MaybeRef<string>, start: Ref<number>) => {
   const matchHistory = ref<MatchInfo[]>([])
   error.value = false
   const getMatchHistory = async () => {
     loading.value = true
     try {
       const matches = await fetch(
-        `${import.meta.env.VITE_URL}/api/get-matches/${puuid
-        }?start=${start.value}&count=5&region=${regionParamToContinentMapper(region)}`,
+        `${import.meta.env.VITE_URL}/api/get-matches/${unref(puuid)
+        }?start=${start.value}&count=5&region=${regionParamToContinentMapper(unref(region))}`,
       )
       const matchesIds = await matches.json() as string[]
       await Promise.allSettled(
         matchesIds.map(async (matchId: string) => {
           return fetch(
-            `${import.meta.env.VITE_URL}/api/get-match/${matchId}?region=${regionParamToContinentMapper(region)}`,
+            `${import.meta.env.VITE_URL}/api/get-match/${matchId}?region=${regionParamToContinentMapper(unref(region))}`,
           ).then(res => res.json())
             .then(match => matchHistory.value.push({ ...match, show: false }))
             .catch(er => console.error(er))

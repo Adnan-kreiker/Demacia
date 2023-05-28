15f950adacc3c4e44634bdde54021a0acf324e19
Adnan Kreiker <akreiker@aedifion.com>
2023-05-28 14:55:03 +0200

    Remove unused files

--- src/modules/virtualScroller.ts
+++
@@ -1,8 +0,0 @@
-// import { UserModule } from '~/types'
-// import { RecycleScroller } from 'vue-virtual-scroller'
-// // import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
-
-// export const install: UserModule = ({ app }) => {
-//   app.component('RecycleScroller', RecycleScroller)
-
-// }
